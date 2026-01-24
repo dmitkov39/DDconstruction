143cf70e46d6a982554df1bf9faea7e4186c7fba
dmitkov39 <dmitkov39@gmail.com>
2026-01-24 14:08:59 +0200

    Update page.tsx

--- src/app/land-leveling-services/page.tsx
+++ src/app/land-leveling-services/page.tsx
@@ -10,7 +10,9 @@ export const metadata: Metadata = {
 export default function LandLevelingServicesPage() {
   return (
     <>
-      <Header />
-    </>
+  <Header />
+  <SiteCleaningInteractive />
+</>
+
   );
 }
