eb19569269a727058dafd3faefea136d8cd7d437
dmitkov39 <dmitkov39@gmail.com>
2026-01-24 14:29:08 +0200

    Update SiteCleaningInteractive.tsx

--- src/app/site-cleaning-services/components/SiteCleaningInteractive.tsx
+++ src/app/site-cleaning-services/components/SiteCleaningInteractive.tsx
@@ -1,25 +1,16 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React from 'react';
 import HeroSection from './HeroSection';
 import ServicesOverview from './ServicesOverview';
 import PricingTimeline from './PricingTimeline';
 
 const SiteCleaningInteractive = () => {
-  const contactFormRef = useRef<HTMLDivElement>(null);
-
-  const scrollToContactForm = () => {
-    contactFormRef?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  };
-
   return (
     <>
-      <HeroSection onGetQuote={scrollToContactForm} />
+      <HeroSection />
       <ServicesOverview />
       <PricingTimeline />
-      <div ref={contactFormRef}>
-        <ContactForm />
-      </div>
     </>
   );
 };
