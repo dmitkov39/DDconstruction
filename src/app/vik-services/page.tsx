7c4c2ae279600569a51d9f801b36412b95682ce0
dmitkov39 <dmitkov39@gmail.com>
2026-07-13 16:48:11 +0300

    Update page.tsx

--- src/app/vik-services/page.tsx
+++ src/app/vik-services/page.tsx
@@ -73,9 +73,9 @@ export default function VikServicesPage() {
               <Image
                 src="/assets/images/20251201_123557-1766602582373.jpg"
                 alt="Изкопана траншея за ВиК връзка"
-                width={1100}
-                height={950}
-                className="rounded-2xl shadow-lg object-cover w-full h-[1100px] lg:h-[950px]"
+                width={900}
+                height={750}
+                className="rounded-2xl shadow-lg object-cover w-full h-[900px] lg:h-[750px]"
                 priority
               />
             </div>
